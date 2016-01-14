25eb64fbd66bc934a3a133bf173bbc7d61110897
Bryce Johnson <brycepj@gmail.com>
2016-01-13 21:03:40 -0500

    Update markup to ng syntax.

--- app/components/tourPanel.component.ts
+++ app/components/tourPanel.component.ts
@@ -5,13 +5,13 @@ import {Component} from 'angular2/core';
 	template: `
     <div id="message-box" class="message-box">
       <span class="mb-title">tour</span>
-      <span class="mb-progress" data-bind="text:tourBox.tourProgress"></span>
-      <p class="mb-content" data-bind="html:tourBox.currentMessage"></p>
-      <a class="mb-action" href="#" target="_blank" data-bind="visible:tourBox.hasButton,text:tourBox.currentXText,attr: {href:tourBox.currentXUrl}"></a>
-      <a class="mb-action" href="#" target="_blank" data-bind="visible:tourBox.currentAction,click:tourAction,text:tourBox.currentXText">do it!</a>
+      <span class="mb-progress">{{tourBox.tourProgress}}</span>
+      <p class="mb-content">{{tourBox.currentMessage}}</p>
+      <a class="mb-action" *ngIf="tourBox.hasButton" (click)="navto=tourBox.currentXUrl"> {{tourBox.currentXText}}</a>
+      <a class="mb-action" (click)="tourAction" *ngIf="tourBox.currentAction">{{tourBox.currentXText}}</a>
       <nav class="mb-nav">
-        <a href="#" data-bind="click:tourPrevious"><i class="fa fa-chevron-left"></i></a>
-        <a href="#" data-bind="click:tourNext"><i class="fa fa-chevron-right"></i></a>
+        <a href="#" (click)="tourPrevious"><i class="fa fa-chevron-left"></i></a>
+        <a href="#" (click)="tourNext"><i class="fa fa-chevron-right"></i></a>
       </nav>
     </div>
 	`
